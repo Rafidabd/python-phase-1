e86a6ccc5d295d2f16334b4daa91fb9bad454b23
rafid <rafidiscooked07@gmail.com>
2025-12-30 01:40:57 +0600

    variable_input output_basic

--- day01_basics/input output_io.py
+++ day01_basics/input output_io.py
@@ -1,3 +1,7 @@
 Name=input("what is your name ? ")
 FavFood= input ("whats your fav food?")
+x,y,z=10,20,30 #assigning multiple values
+p=float(3.1416) #this is called casting
 print(Name + ' likes ' + FavFood)
+print(x,y,z)
+print(x,Name) # here we cant use + as both are different data types ,can be done to two strings
--- day01_basics/io_age.py
+++ day01_basics/io_age.py
@@ -1,3 +1,4 @@
 birth_year=input("whats your birth year?")
 age=2025-int(birth_year)
+print(type(age))
 print(age)
