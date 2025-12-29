a91d72eab1ebd6e1d60b6208b5f3deefc0c9b184
rafid <rafidiscooked07@gmail.com>
2025-12-29 23:58:19 +0600

    Day 1: variablest

---
+++ day01_basics/input output_io.py
@@ -0,0 +1,3 @@
+Name=input("what is your name ? ")
+FavFood= input ("whats your fav food?")
+print(Name + ' likes ' + FavFood)
---
+++ day01_basics/io_age.py
@@ -0,0 +1,3 @@
+birth_year=input("whats your birth year?")
+age=2025-int(birth_year)
+print(age)
