526d89a2de1a6a8af4221ec4aa7e11633e5731fa
rafid <rafidiscooked07@gmail.com>
2026-01-13 16:48:15 +0600

    LOOPS_ADVANCED PROBLEMS_SOLVEDD

--- day03_ loops/problem_solving_advanced.py
+++ day03_ loops/problem_solving_advanced.py
@@ -1,72 +1,72 @@
-# ###armstrong number checking:
+###armstrong number checking:
 
 
-# n=int(input("please enter any number:"))
-# s=0
-# x=n
+n=int(input("please enter any number:"))
+s=0
+x=n
 
-# while x>0:
-#     dig=x%10
-#     s=s+pow(dig,3)
+while x>0:
+    dig=x%10
+    s=s+pow(dig,3)
 
-#     x=x//10
+    x=x//10
 
-# if (s==n):
-#     print("armstrong number")
-# else:
-#     print("not armstrong")
-
-
-
+if (s==n):
+    print("armstrong number")
+else:
+    print("not armstrong")
 
 
 
 
 
-# ########Palindrome Number checking:
 
-# n=int(input("please enter any number"))
 
-# x=n
-# rev=0
 
-# while x>0:
-#     dig=x%10
-#     rev=rev*10+dig
-#     x=x//10
+########Palindrome Number checking:
 
-# if (rev==n):
-#     print("pallindrome")
-# else:
-#     print("not pallindrome")
+n=int(input("please enter any number"))
+
+x=n
+rev=0
+
+while x>0:
+    dig=x%10
+    rev=rev*10+dig
+    x=x//10
+
+if (rev==n):
+    print("pallindrome")
+else:
+    print("not pallindrome")
     
 
 
 
 
 
-# #####Password attempt:
+#####Password attempt:
 
 
 
-# correct_pass= 1234
+correct_pass= 1234
 
 
 
 
-# count=0
+count=0
 
-# while count <3:
-#     password=int(input("please enter your password"))
+while count <3:
+    password=int(input("please enter your password"))
     
-#     if password==correct_pass:
-#         print("access granted")
-#         break
-#     else:
-#         print("wrong password.enter again")
-#         count=count+1
-# if count==3:
-#     print("access denied")
+    if password==correct_pass:
+        print("access granted")
+        break
+    else:
+        print("wrong password.enter again")
+        count=count+1
+if count==3:
+    print("access denied")
 
 
 
