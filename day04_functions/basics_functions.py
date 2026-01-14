be7afbe390f597f462bef0ed8d1caa54cc0993b3
rafid <rafidiscooked07@gmail.com>
2026-01-15 00:51:34 +0600

    functions_basics

---
+++ day04_functions/basics_functions.py
@@ -0,0 +1,74 @@
+def rafid_func():
+    print("Hello.This is rafid abdullah") ##if we call this function,this will happen
+#####now we have set up our function
+
+
+rafid_func() ###calling the function
+
+
+
+####returning something:
+
+def rafid_func () :
+    return 'hello rafid.'
+
+print(rafid_func())  ####this will return as string
+
+print(len('test'))  ####will return 4
+
+
+
+
+
+##arguements and parametres:
+
+
+def porte_bosh(name,age):  ###parametre set 
+    print("ei shala tor syllabus sesh hoise?")
+    print(f"{name},porte bosh vai.")
+    print(f"everyone expects a lot from you..your  age is {age}")
+
+porte_bosh("rafid","19") ###arguements passed..******Order matters
+porte_bosh("Masfi","18")
+
+
+
+#######Returning in function:
+
+#return basically means . 1)the function will do its work and end 2) then it will transform into what we have wanted
+##basically think of it as the function being turned into something
+
+
+
+
+
+def add (x,y) :
+    z=x+y
+    return z
+def sub (x,y) :
+    z=x-y
+    return z
+def mul (x,y) :
+    z=x*y
+    return z
+def div (x,y) :
+    z=x/y
+    return z
+
+
+print(add(1,2))  ##basically means print(3)
+print(sub(1,2))
+print(mul(1,2))
+print(div(1,2))
+
+
+
+
+
+
+
+
+
+
+
+
