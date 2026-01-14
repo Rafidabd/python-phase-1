bb40bfb64ef268bf9d9d1cac65c30d82718ee5b4
rafid <rafidiscooked07@gmail.com>
2026-01-15 01:32:51 +0600

    functions_level 1_problem solved

---
+++ day04_functions/problems_level 1.py
@@ -0,0 +1,46 @@
+# ####adder function:
+
+
+# def func_add(x,y):
+#     z=x+y
+#     return z
+
+
+# a,b=map(int,input("please enter two numbers").split())
+# print(func_add(a,b))
+
+
+
+
+# ####square function:
+
+# def func_sqr(x):
+#     z=pow(x,0.5)
+#     return z
+
+
+# a=int(input("please enter your number"))
+
+# print(func_sqr(a))
+
+
+
+
+#####even or odd:
+
+
+def numb_checker(x):
+    if x%2==0:
+        return "even"
+    else:
+        return "odd"
+    
+
+a=int(input("please enter your number"))
+print(numb_checker(a))
+
+
+
+
+
+
