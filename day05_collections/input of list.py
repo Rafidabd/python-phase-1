d6c2827946ae4aea1ffac9bc6470aa7ccc709482
rafid <rafidiscooked07@gmail.com>
2026-01-17 00:51:20 +0600

    list_inputs

---
+++ day05_collections/input of list.py
@@ -0,0 +1,32 @@
+
+# ####using loops:
+
+
+
+# n=int(input("please enter the range of numbers:"))
+
+
+# num=[]
+
+# for x in range (n):
+#     y=int((input("please enter your number")))
+#     num.append(y)
+# print (num)
+
+
+
+
+
+######using split:
+
+
+nums=list(map(int,input("please enter your numbers").split()))
+print(nums)
+
+
+
+
+
+
+
+
---
+++ day05_collections/looping_and_methods.py
@@ -0,0 +1,48 @@
+names=["rafid","masfi","nayan","hasnin","jahan"]
+
+for x in names:
+    print(x)
+for x in enumerate(names):
+    print(x)  ###we will get a tuple here
+
+
+
+####Adding item:
+
+names.append("rafid")
+print(names)
+
+names.insert(2,"habib")
+print(names)
+
+
+
+
+###Removing items:
+
+
+
+names.pop()
+print(names)
+
+
+names.pop(2)
+print(names)  ##3exact one
+
+names.clear()
+print(names)   ###all elements removed
+
+del names [0:2] #####for removing more than one element
+print(names)
+
+
+
+#####Finding objects:
+
+
+
+
+print(names.count("rafid")) ####the number of occurance
+
+
+
