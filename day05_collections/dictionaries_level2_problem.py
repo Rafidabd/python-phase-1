0d2ec2be688761bfd5c7d67dcea60c08839d8707
rafid <rafidiscooked07@gmail.com>
2026-01-22 18:12:43 +0600

    dictionaries_level 2_problem_solved

---
+++ day05_collections/dictionaries_level2_problem.py
@@ -0,0 +1,88 @@
+
+######1-Frequency counter:
+
+n = int(input("Please enter the number of elements: "))
+numbers = list(map(int, input("Please enter the numbers: ").split()))
+
+frequency={}
+
+for x in numbers:
+    if x not in frequency:
+        frequency[x]=1
+    else:
+    
+        frequency[x]=frequency[x]+1
+
+
+for key,value in frequency.items():
+    print(key,value)
+
+
+
+
+##Character frequency:
+
+char = list( input("Please enter the your character: "))
+
+frequency={}
+
+
+for x in char:
+     if x not in frequency:
+        frequency[x]=1
+     else:
+    
+        frequency[x]=frequency[x]+1
+
+
+for key,value in frequency.items():
+    print(key,value)
+
+
+#3-word frequency:
+
+
+sent = list( input("Please enter the your sentence: ").split())
+frequency={}
+for x in sent:
+     if x not in frequency:
+        frequency[x]=1
+     else:
+    
+        frequency[x]=frequency[x]+1
+
+
+for key,value in frequency.items():
+    print(key,value)
+
+
+
+
+
+    
+        
+
+
+
+
+
+     
+
+
+
+
+
+
+
+
+
+
+   
+
+
+
+
+
+
+
+    
